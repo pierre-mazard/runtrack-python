e97e3e2998797b3d26ec04a40c350476396097a0
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-20 14:02:39 +0100

    job07

---
+++ jour01/job07/somme_de_deux_variables.py
@@ -0,0 +1,11 @@
+# Somme de deux variables
+
+
+    #Attribution des variables
+num1 = int(40)
+num2 = int(2)
+
+    #Opération sur les deux variables
+somme = num1 + num2
+
+print("La sommme de",  num1,  "et de",  num2, "est égale à ", somme)
