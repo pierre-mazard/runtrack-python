6412586923e5853ea3ef8966e64e72be9714d506
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-20 15:20:40 +0100

    Modification erreur job09 mis en job08 et ajout du job08

---
+++ jour01/job08/produit_variables.py
@@ -0,0 +1,11 @@
+# Produit de deux variables
+
+#Attribution des variables
+num1 = 3
+num2 = 14
+
+#Produit des deux variables
+produit = num1 * num2
+
+# Affichage du résultat
+print ("Le produit de" , num1, "par", num2, "est égal à : ", produit)
