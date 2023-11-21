60222441a59351ddbe102f31590d035dcd5d6dc5
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-21 10:30:33 +0100

    job02

---
+++ jour02/job02/parcourir_nombres_un_sur_deux.py
@@ -0,0 +1,13 @@
+#Message
+print ("""   
+       Affichage d'un nombres sur deux de 0 à 20 
+       """)
+
+#Attribution des variables
+
+nombres = range(0, 21, 2)
+
+#Boucle
+
+for x in nombres:
+    print(x)
