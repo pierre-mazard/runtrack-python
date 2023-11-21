9f3f5d074c6064e19816ed97ca6f4bdf94af2897
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-21 10:13:48 +0100

    job01

---
+++ jour02/job01/parcourir_nombres.py
@@ -0,0 +1,15 @@
+#Script qui parcrout les nombres de 0 à 20 et les affichent dans le temrinal.
+
+#Message
+print ("""   
+       Affichage des nombres de 0 à 20 
+       """)
+
+#Attribution des variables
+
+nombres = range(0, 20 +1)
+
+#Boucle
+
+for x in nombres:
+    print(x)
