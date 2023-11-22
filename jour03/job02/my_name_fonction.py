cb84183e3a4edcd14134dd6b35474cd6ad676ae5
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-22 10:21:27 +0100

    job02 fonction my name

---
+++ jour03/job02/my_name_fonction.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 10:01:28 2023
+
+@author: Mazard Pierre
+
+Programme d'introduction aux fonctions
+"""
+
+#Importation de fonctions externes (librairies) :
+    
+#Définition locale de fonctions : 
+
+def my_print_name (name):
+    print (""""
+Votre nom est :""", name)
+    
+#Déclaration des variables : 
+name = input("Quel est votre nom ? : ")
+#Corps principal du programme : 
+
+my_print_name(name)    
