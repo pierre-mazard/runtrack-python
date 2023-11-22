1ab590534a19544532388534c10f3e46424822c3
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-22 11:04:47 +0100

    job04 fonction/return

---
+++ jour03/job04/gethello.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 10:01:28 2023
+
+@author: Mazard Pierre
+
+Programme d'introduction aux fonctions
+"""
+
+#Importation de fonctions externes (librairies) :
+    
+#Définition locale de fonctions : 
+
+def gethello ():
+    return ("Hello la Plateforme")
+
+    
+#Déclaration des variables : 
+    
+#Corps principal du programme : 
+    
+print (gethello ())
