0d64df81e36354241f967ec024dd93aa3ed16234
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-22 14:12:14 +0100

    pour aller plus loin inversion de chaine

---
+++ jour03/pour aller plus loin/inversion.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 10:01:28 2023
+
+@author: Mazard Pierre
+
+Programme d'introduction aux fonctions
+"""
+
+#Importation de fonctions externes (librairies) :
+    
+#Définition locale de fonctions : 
+
+def inversion(chaine):
+    return chaine[::-1]
+    
+#Déclaration des variables : 
+    
+#Corps principal du programme : 
+
+print(inversion("nikana"))
+print(inversion("soleil"))
+print(inversion("coucou"))
+print(inversion("noisrevni"))
