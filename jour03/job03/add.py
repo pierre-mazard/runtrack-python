d59083a0b4a804153a6457a9b43bc34bd237b61f
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-22 10:44:30 +0100

    job 03 Additions

---
+++ jour03/job03/add.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 10:01:28 2023
+
+@author: Mazard Pierre
+
+Programme d'introduction aux fonctions
+"""
+
+#Importation de fonctions externes (librairies) :
+    
+#Définition locale de fonctions : 
+
+def add (num1, num2):
+    somme = num1 + num2
+    print (f"La somme de {num1} et {num2} est égale à : {somme}")
+
+    
+#Déclaration des variables : 
+    
+#Corps principal du programme : 
+
+add(4,5)
+add(125,30)
+add(8,9)
+add(56,-6)   
+    
