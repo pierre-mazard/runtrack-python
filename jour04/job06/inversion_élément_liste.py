0bf5d13be0d6769797fc541d69546de05327fb2e
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-23 11:31:14 +0100

    job06 inversion de valeurs dans une liste

---
+++ jour04/job06/inversion_élément_liste.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 11:12:42 2023
+
+@author: Mazar
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 23 2023
+
+@author: Mazard Pierre
+
+
+"""
+
+#Importation de fonctions externes (librairies) :
+    
+#Définition locale de fonctions : 
+
+        
+#Déclaration des variables : 
+ 
+L = [1,2,3,4,5]
+
+#Corps principal du programme : 
+
+print (L)
+L[0], L[-1] = L[-1], L[0]
+print (L)
