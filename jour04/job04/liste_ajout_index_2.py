cfe8f12e07ba0811218faebcd98804e9330281b2
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-23 11:14:35 +0100

    job04 ajout élément index 2

---
+++ jour04/job04/liste_ajout_index_2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 11:12:42 2023
+
+@author: Mazar
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 23 2023
+
+@author: Mazard Pierre
+
+
+"""
+
+#Importation de fonctions externes (librairies) :
+    
+#Définition locale de fonctions : 
+
+def liste ():
+    fruits = ['pomme','cerise','orange']
+    fruits.insert(2, "Mangue")
+    return fruits
+
+#Déclaration des variables : 
+    
+#Corps principal du programme : 
+print(liste())    
