ae374920701090952b1ed8c579aac762c416d84e
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-23 15:17:22 +0100

    job13 supression doublons dans la liste

---
+++ jour04/job13/doublons.py
@@ -0,0 +1,25 @@
+
+"""
+Created on Wed Nov 23 2023
+
+@author: Mazard Pierre
+
+
+"""
+
+#Importation de fonctions externes (librairies) :
+    
+#Définition locale de fonctions : 
+
+def suppression_doublons (L):
+    for i in L:
+        if i not in L2:
+            L2.append(i)
+    print(L2)
+        
+#Déclaration des variables : 
+L = [10,20,30,20,10,50,60,40,80,50,40]
+L2 = []
+#Corps principal du programme : 
+
+suppression_doublons(L)
