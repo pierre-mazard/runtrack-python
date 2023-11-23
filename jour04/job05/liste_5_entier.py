6aa84c7fd0d44cb32cd7a6a4554dcd9762711795
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-23 11:26:42 +0100

    job05 liste de 5 entierrs et fonction midifiant la liste

---
+++ jour04/job05/liste_5_entier.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 11:12:42 2023
+
+@author: Mazar
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 23 2023
+
+@author: Mazard Pierre
+
+
+"""
+
+#Importation de fonctions externes (librairies) :
+    
+#Définition locale de fonctions : 
+
+def somme ():
+    L[3] = L[2] + L[4]
+    print (L)
+    print (L[4])
+        
+#Déclaration des variables : 
+ 
+L = [1,2,3,4,5]
+
+#Corps principal du programme : 
+print (L)
+print (L[1]) 
+somme()
