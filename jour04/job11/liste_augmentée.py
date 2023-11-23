81211ab2fd88cce4049404fe7ab7f9e0e6c628b7
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-23 13:20:35 +0100

    job11 liste modifée en augmentant la valeurs de chaque élément de +1

---
+++ jour04/job11/liste_augmentée.py
@@ -0,0 +1,27 @@
+
+"""
+Created on Wed Nov 23 2023
+
+@author: Mazard Pierre
+
+
+"""
+
+#Importation de fonctions externes (librairies) :
+    
+#Définition locale de fonctions : 
+  
+     
+#Déclaration des variables : 
+ 
+L = [7, 11, 42, 39, 2]
+L_plus_1 = [i + 1 for i in L]
+
+#Corps principal du programme : 
+
+print(f"""
+{L}""")
+print(f"""
+Liste modifée en augmentant de 1 la valeur de chaques élément :
+      
+{L_plus_1}""")
