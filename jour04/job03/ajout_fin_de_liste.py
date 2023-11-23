637eacecc26c1c83980f5cea89f3f0ae0f36cebf
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-23 11:12:29 +0100

    job03 ajout fin de liste

---
+++ jour04/job03/ajout_fin_de_liste.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 23 2023
+
+@author: Mazard Pierre
+
+
+"""
+
+#Importation de fonctions externes (librairies) :
+    
+#Définition locale de fonctions : 
+
+def liste ():
+    fruits = ['pomme','cerise','orange']
+    fruits.append("Melon")
+    return fruits
+
+#Déclaration des variables : 
+    
+#Corps principal du programme : 
+print(liste())    
