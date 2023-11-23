80a2f0194757509ae70a42382a28d5d2e46e8b33
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-23 11:04:36 +0100

    job02 sélection d'un élément dans la liste

---
+++ jour04/job02/sélection_élément_liste.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 23 2023
+
+@author: Mazard Pierre
+
+
+"""
+
+#Importation de fonctions externes (librairies) :
+    
+#Définition locale de fonctions : 
+
+def liste ():
+    fruits = ['pomme','cerise','orange']
+    return fruits [1]
+
+#Déclaration des variables : 
+    
+#Corps principal du programme : 
+print(liste())    
