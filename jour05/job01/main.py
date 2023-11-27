b95408fc1fdfc77e3f86f7cb1723256e4fba3e18
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-27 10:45:30 +0100

    Modification erreur de fichier

---
+++ jour05/job01/main.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Nov 27 2023
+
+@author: Mazard Pierre
+
+
+"""
+
+#Importation de fonctions externes (librairies) :
+    
+    
+#Définition locale de fonctions : 
+  
+    
+#Déclaration des variables : 
+
+prenom = input("""Entrez votre prénom : 
+=> """)    
+
+#Corps principal du programme : 
+
+print (f"""
+Hello {prenom} !""")
